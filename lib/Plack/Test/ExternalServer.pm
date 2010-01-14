c6fd7258dc68dd56ab7c812ba32914c1bbb2141a
Florian Ragwitz <rafl@debian.org>
2010-01-14 09:15:20 +0100

    Very basic docs.

--- lib/Plack/Test/ExternalServer.pm
+++ lib/Plack/Test/ExternalServer.pm
@@ -2,10 +2,19 @@ use strict;
 use warnings;
 
 package Plack::Test::ExternalServer;
+# ABSTRACT: Run HTTP tests on external live servers
 
 use URI;
 use Carp;
 use LWP::UserAgent;
+
+=head1 SYNOPSIS
+
+    $ PLACK_TEST_IMPL=Plack::Test::ExternalServer \
+      PLACK_TEST_EXTERNALSERVER_URI=http://myhost.example/myapp/ \
+      perl my_plack_test.t
+
+=cut
 
 sub test_psgi {
     my %args = @_;
