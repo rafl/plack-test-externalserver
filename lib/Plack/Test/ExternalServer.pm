9a0c9996aff84f38d496b09c52c59a701bad0636
Florian Ragwitz <rafl@debian.org>
2010-01-14 09:09:38 +0100

    Let the env take precedence over the passed in URIs.

--- lib/Plack/Test/ExternalServer.pm
+++ lib/Plack/Test/ExternalServer.pm
@@ -11,8 +11,8 @@ sub test_psgi {
     my %args = @_;
 
     my $client = delete $args{client} or croak 'client test code needed';
-    my $ua     = delete $args{ua}  || LWP::UserAgent->new;
-    my $base   = delete $args{uri} || $ENV{PLACK_TEST_EXTERNALSERVER_URI};
+    my $ua     = delete $args{ua} || LWP::UserAgent->new;
+    my $base   = $ENV{PLACK_TEST_EXTERNALSERVER_URI} || delete $args{uri};
        $base   = URI->new($base) if $base;
 
     $client->(sub {
